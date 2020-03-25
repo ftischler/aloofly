77cc3ed4332f869e57a451a6c4166ff6b310929b
Florian Tischler <florian.tischler@gmx.net>
2020-03-25 15:43:01 +0100

    Add ngrx router store

--- apps/aloofly/src/app/app.module.ts
+++ apps/aloofly/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, StoreRouterConnectingModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })

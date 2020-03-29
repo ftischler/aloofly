2866d170403c5882fda0cf138160dcd40287c5dd
Florian Tischler <florian.tischler@gmx.net>
2020-03-29 21:46:19 +0200

    Fixed some bugs

--- apps/mws30/src/app/common/get-attacked-player.ts
+++ apps/mws30/src/app/common/get-attacked-player.ts
@@ -1,7 +1,7 @@
-import { GameContext, Player } from '@aloofly/mws30-models';
+import { Game, Player } from '@aloofly/mws30-models';
 import { getCurrentTurn } from './get-current-turn';
 
-export function getAttackedPlayer({game, player}: GameContext): Player | undefined {
+export function getAttackedPlayer(game: Game, player: Player): Player | undefined {
   const currentTurn = getCurrentTurn(game, player);
   const attackingPlayerId: string | undefined = currentTurn?.attackingPlayerId;
   return attackingPlayerId ? game.players[attackingPlayerId] : undefined;
--- apps/mws30/src/app/shared/attacked-player.pipe.ts
+++ apps/mws30/src/app/shared/attacked-player.pipe.ts
@@ -1,12 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { GameContext, Player } from '@aloofly/mws30-models';
+import { Game, Player } from '@aloofly/mws30-models';
 import { getAttackedPlayer } from '../common/get-attacked-player';
 
 @Pipe({
   name: 'attackedPlayer'
 })
 export class AttackedPlayerPipe implements PipeTransform {
-  transform(ctx: GameContext): Player | undefined {
-    return getAttackedPlayer(ctx);
+  transform(game: Game, player: Player): Player | undefined {
+    return getAttackedPlayer(game, player);
   }
 }
